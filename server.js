c16bfbc710c003a5d7967aed8cedef057e2d0449
Jonnel Latham <Jonnel Latham>
2016-04-20 03:54:25 -0400

    To Do List working

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    logger = require('morgan')('dev'),
+    server = express();
+
+server.use(express.static(__dirname));
+server.use(logger);
+
+server.get('/', function(req, res){
+  res.sendFile('/index.html', {root: __dirname});
+});
+
+server.listen(8080, function(){
+  console.log('Now listening on port '+ 8080);
+});
